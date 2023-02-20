f82ee6f3d9092ec55f9f5eabbd15efcf4e6052ea
Mikhelmariya <mikhela65@gmail.com>
2023-02-20 08:15:06 +0530

    http package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,6 +14,11 @@ void getLocation() async {
   print(location.latitude);
   print(location.longitude);
 }
+
+// void getData() async {
+//   http.Response response = await http.get('API for weather');
+//   print(response.body);
+// }
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
